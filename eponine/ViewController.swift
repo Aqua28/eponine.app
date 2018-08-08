b6f593e852e743c1a4735804ba5ec150b5081267
aqua28 <aidyl889@gmail.com>
2018-08-08 15:02:54 -0700

    added adding calculator

--- eponine/ViewController.swift
+++ eponine/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var result: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
     @IBOutlet weak var start_button: UIButton!
+    
     @IBAction func start_stylin_tapped(_ sender: Any) {
         start_button.setTitle("loading", for: .normal)
-        start_button.
+        
+        print(text1.text!)
+        print(text2.text!)
+        
+        result.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+        
+        
     }
+
+     
+        
+    
+  
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
